3f4a726b4422d7355bd4f6708e28c5bb997e07bf
Michal Racko <rackom10@gmail.com>
2015-12-17 12:09:41 +0100

    uprava

--- app/controllers/reuters_controller.rb
+++ app/controllers/reuters_controller.rb
@@ -1,8 +1,10 @@
 class ReutersController < ApplicationController
 # list all
 	def find
+		page = "<b>"+params[:name]+"</b>"
+
 		respond_to do |format|
-			format.xml { render xml: "<b>"+params[:name]+"</b>", status: :ok }
+			format.xml { render xml: page, status: :ok }
 		end
 	end
 end
