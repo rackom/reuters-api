f19c0c60f0cf1b271f1925d92f2d604efbfe88ba
Michal Racko <rackom10@gmail.com>
2015-12-17 12:23:04 +0100

    realny kod

--- app/controllers/reuters_controller.rb
+++ app/controllers/reuters_controller.rb
@@ -1,10 +1,15 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
 class ReutersController < ApplicationController
 # list all
 	def find
-		page = "<b>"+params[:name]+"</b>"
+		page = Nokogiri::HTML(open("http://uk.reuters.com/business/quotes/overview?symbol=" + params[:name]))   
+		result = page.css('div#overallRatios')
 
 		respond_to do |format|
-			format.xml { render xml: page, status: :ok }
+			format.xml { render xml: result, status: :ok }
 		end
 	end
 end
