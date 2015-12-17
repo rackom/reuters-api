ec5014b547bf8d1fde45a61705cfe71b16ca7a49
Michal Racko <rackom10@gmail.com>
2015-12-17 14:55:39 +0100

    realny kod

--- app/controllers/reuters_controller.rb
+++ app/controllers/reuters_controller.rb
@@ -4,12 +4,39 @@ require 'open-uri'
 
 class ReutersController < ApplicationController
 # list all
-	def find
-		page = Nokogiri::HTML(open("http://uk.reuters.com/business/quotes/overview?symbol=" + params[:name]))   
-		result = page.css('div#overallRatios')
+	def overall_ratios
+		result = fetch_element_by_css('div#overallRatios')		
 
 		respond_to do |format|
 			format.xml { render xml: result, status: :ok }
 		end
+	end
+
+	def chart
+		result = fetch_element_by_css('div#companyOverviewChart')		
+
+		respond_to do |format|
+			format.xml { render xml: result, status: :ok }
+		end
+	end
+
+	def competitors
+		tmp = fetch_element_by_css('div.module')
+
+		result = ""
+		tmp.each do |node|
+			if node.content.include?('Competitors')
+				result = node
+			end
+		end
+
+		respond_to do |format|
+			format.xml { render xml: result, status: :ok }
+		end
+	end
+
+	def fetch_element_by_css(selector)
+		page = Nokogiri::HTML(open("http://uk.reuters.com/business/quotes/overview?symbol=" + params[:name]))   
+		page.css(selector)
 	end
 end
