7a11303d17ce24112ee6bb49a21eb5715f1730cd
Michal Racko <rackom10@gmail.com>
2015-12-18 12:13:09 +0100

    pridana testovacia metoda na vypis popisu o company

--- app/controllers/reuters_controller.rb
+++ app/controllers/reuters_controller.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'AlchemyAPI'
 
 class ReutersController < ApplicationController
 # list all
 	def overall_ratios
-		result = fetch_element_by_css('div#overallRatios')		
+		result = fetch_element_by_css("http://uk.reuters.com/business/quotes/overview?symbol=#{params[:name]}", 'div#overallRatios')		
 
 		respond_to do |format|
 			format.xml { render xml: result, status: :ok }
@@ -13,7 +14,7 @@ class ReutersController < ApplicationController
 	end
 
 	def chart
-		result = fetch_element_by_css('div#companyOverviewChart')		
+		result = fetch_element_by_css("http://uk.reuters.com/business/quotes/overview?symbol=#{params[:name]}", 'div#companyOverviewChart')		
 
 		respond_to do |format|
 			format.xml { render xml: result, status: :ok }
@@ -21,7 +22,7 @@ class ReutersController < ApplicationController
 	end
 
 	def competitors
-		tmp = fetch_element_by_css('div.module')
+		tmp = fetch_element_by_css("http://uk.reuters.com/business/quotes/overview?symbol=#{params[:name]}", 'div.module')
 
 		result = ""
 		tmp.each do |node|
@@ -33,10 +34,43 @@ class ReutersController < ApplicationController
 		respond_to do |format|
 			format.xml { render xml: result, status: :ok }
 		end
+	end	
+
+	def fetch_element_by_css(url, selector)
+		page = Nokogiri::HTML(open(url))	 
+		page.css(selector)
 	end
 
-	def fetch_element_by_css(selector)
-		page = Nokogiri::HTML(open("http://uk.reuters.com/business/quotes/overview?symbol=" + params[:name]))   
-		page.css(selector)
+	#######
+
+	def test
+		@result = fetch_element_by_css("http://uk.reuters.com/business/quotes/companyProfile?symbol=#{params[:name]}", "div#companyNews div.moduleBody").to_s
+		
+		words = Array.new()
+		color_hash = Hash.new()
+		alchemyapi = AlchemyAPI.new()
+
+		response = alchemyapi.entities('text', @result, { 'sentiment'=>0 })
+
+		if response['status'] == 'OK'
+			for entity in response['entities']
+				
+				if color_hash[entity['type']].nil?
+					r = rand(255)
+					g = rand(255)
+					b = rand(255)
+
+					color_hash[entity['type']] = "#{r}, #{g}, #{b}"
+				end
+
+				words << [entity['text'], entity['type'], color_hash[entity['type']]]
+			end
+		end
+
+		words.each { |word| @result.gsub!(/\b(#{word[0]})\b/i, '<span style="border: 1px solid rgb(' + word[2] + '); padding: 0.2em 0.5em; background-color: rgb(' + word[2] + '); color: white; opacity: 1;">\1</span>')}
+
+		# respond_to do |format|
+		# 	format.xml { render xml: @result.html_safe, status: :ok }
+		# end
 	end
 end
